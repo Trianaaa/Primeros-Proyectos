561547f4d504351159e8959b093691f1da93a64c
Trianaaa <59985270+Trianaaa@users.noreply.github.com>
2022-09-06 22:05:01 -0500

    Add files via upload

---
+++ Curso_JS_Basico/Leccion_2/notacion.js
@@ -0,0 +1,27 @@
+// Notacion
+// ; = Delimitar el final de una linea
+const b = 4;
+b + 4;
+
+// . = Se utiliza en los objetos para acceder a los atributos
+console.clear
+
+// [] = Se utilizan para las listas arreglos o arrays
+const ar=[1, 2, 3, 4]
+console.log(ar[2])
+
+// () = Se usan para las funciones
+function suma (a,b){
+    // Se escribe la funcion
+}
+
+// {} = Se usan para objetos, funciones y estructuras de control
+const movil={
+    anchura:5,
+    altura:10
+}
+
+if (true){
+    // Todo lo que halla entre llaves
+    const constante2 = "Hola"
+}
